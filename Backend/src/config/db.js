f1f2c53e2aab3172927ed9ecb8d76c34a7b91b41
Ios12316 <idowuolakunlesamuel@gmail.com>
2026-06-10 11:55:17 +0100

    fix case-sensitive transaction import and add mongoose connection error listener

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -8,6 +8,10 @@ try {
     console.warn("Could not set DNS servers:", e);
 }
 
+mongoose.connection.on("error", (err) => {
+    console.error("Mongoose connection error event:", err);
+});
+
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
--- Backend/src/controllers/transactionController.js
+++ Backend/src/controllers/transactionController.js
@@ -1,4 +1,4 @@
-import Transaction from "../models/transaction.js";
+import Transaction from "../models/Transaction.js";
 
 export const addTransaction = async (req, res) => {
     try {
